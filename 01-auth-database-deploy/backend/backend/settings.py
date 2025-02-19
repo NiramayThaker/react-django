2f849bd4752d386efc79c8d789fe03d2b6115a17
NiramayThaker <thakerniramay@gmail.com>
2025-02-19 17:45:43 +0530

    Creating generic views

--- 01-auth-database-deploy/backend/backend/settings.py
+++ 01-auth-database-deploy/backend/backend/settings.py
@@ -41,10 +41,10 @@ REST_FRAMEWORK = {
     ],
 }
 
-SIMPLE_JWT = [{
+SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-}]
+}
 
 # Application definition
 
