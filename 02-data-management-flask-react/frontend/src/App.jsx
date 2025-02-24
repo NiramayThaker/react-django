37b7e45e8e45502520670350dcdbf06cfee331d7
NiramayThaker <thakerniramay@gmail.com>
2025-02-24 18:16:29 +0530

    Completed user data management

--- 02-data-management-flask-react/frontend/src/App.jsx
+++ 02-data-management-flask-react/frontend/src/App.jsx
@@ -1,36 +1,56 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import ContactList from './ContactList'
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
   useEffect(() => {
     fetchContacts()
-  }, [])
+  }, []);
 
   const fetchContacts = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/contacts');
-      const data = await response.json();
-      console.log("API Response:", data); // Debugging
-      if (data && Array.isArray(data.contacts)) {
-        setContacts(data.contacts);
-      } else {
-        setContacts([]); // Fallback if data.contacts is not an array
-      }
-    } catch (error) {
-      console.error("Error fetching contacts:", error);
-      setContacts([]); // Prevents undefined error
-    }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
   };
-  
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
 
   return (
     <>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
