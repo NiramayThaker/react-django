4d9b6ac7cfa435b110bc71b153d6832a88c46a1e
NiramayThaker <thakerniramay@gmail.com>
2025-02-25 08:45:11 +0530

    Setting up routes

--- 03-fastAPI-react-user-authentication/backend/main.py
+++ 03-fastAPI-react-user-authentication/backend/main.py
@@ -4,4 +4,40 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
+
+class Fruit(BaseModel):
+    name: str
+
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
+
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+memory_db = {"fruits": []}
+
+
+@app.get("/fruits", response_model=Fruits)
+def read_fruits():
+    return Fruits(fruits=memory_db["fruits"])
+
+@app.post("/fruits", response_model=Fruit)
+def add_fruit(fruit: Fruit):
+    memory_db["fruits"].append(fruit)
+    return fruit
+
