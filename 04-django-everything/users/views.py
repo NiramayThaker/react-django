62811fc7f69ab65b4f68c408979f94afde73a9b0
NiramayThaker <thakerniramay@gmail.com>
2025-03-02 17:57:29 +0530

    User Registration

--- 04-django-everything/users/views.py
+++ 04-django-everything/users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from .models import Account
 
 # Create your views here.
+def register(request):
+    form = RegistrationForm()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+
+            user = Account.object.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number 
+            user.save()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'user/register.html', context=context)
+
+def login(request):
+    return render(request, 'user/login.html')
+
+def logout(request):
+    return 
